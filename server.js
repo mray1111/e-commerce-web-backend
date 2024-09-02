7dc3e4304e0fcd93bf1362e7cc58fe992436a9f1
mray1111 <manishcode111@gmail.com>
2024-09-02 14:44:43 +0530

    cors added

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const app = require("./app");
-const dotenv=require("dotenv");
-const cloudinary=require("cloudinary");
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+const dotenv = require('dotenv');
+const cloudinary = require('cloudinary');
 
-const connectDatabase=require("./config/database.js");
+const connectDatabase = require('./config/database.js');
+const app = express();
+
 // Handling Uncaught Exception
-process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Uncaught Exception`);
-    process.exit(1);
-  });
+process.on('uncaughtException', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
 
-//config
-  dotenv.config({path:"backend/config/config.env"});
+// Config
+dotenv.config({ path: 'backend/config/config.env' });
 
-//connecting to database
-
+// Connecting to Database
 connectDatabase();
 
-const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME || "dbzkygnbh";
-const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "433441131784874";
-const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "ErfgidwpufrSCjAjuWx8gQS8f3Q";
+const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME || 'dbzkygnbh';
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || '433441131784874';
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || 'ErfgidwpufrSCjAjuWx8gQS8f3Q';
 const PORT = process.env.PORT || 4000;
 
 cloudinary.config({
@@ -28,19 +30,21 @@ cloudinary.config({
   api_secret: CLOUDINARY_API_SECRET,
 });
 
+// Use cors middleware with default settings
+app.use(cors());
+//  routes and other middleware here
+app.use(express.json());
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is working on http://localhost:${PORT}`);
-  });
+  console.log(`Server is working on http://localhost:${PORT}`);
+});
 
- // console.log(hereerror)
-  
-  // Unhandled Promise Rejection
-  process.on("unhandledRejection", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
-  
-    server.close(() => {
-      process.exit(1);
-    });
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
   });
+});
